7d70acd170bbb9e3c698144b3c42c61714b4d255
Matt Travi <programmer@travi.org>
2019-09-03 08:21:28 -0500

    feat(integration-tests): copied required files 4 a canary cucumber test

--- src/testing-test.js
+++ src/testing-test.js
@@ -1,25 +1,48 @@
+import {resolve} from 'path';
+import {promises} from 'fs';
 import sinon from 'sinon';
 import {assert} from 'chai';
+import any from '@travi/any';
+import * as mkdir from '../thirdparty-wrappers/make-dir';
 import scaffoldTesting from './testing';
 
 suite('testing', () => {
   let sandbox;
+  const projectRoot = any.string();
+  const pathToCreatedDirectory = any.string();
 
   setup(() => {
     sandbox = sinon.createSandbox();
+
+    sandbox.stub(mkdir, 'default');
+    sandbox.stub(promises, 'copyFile');
   });
 
   teardown(() => sandbox.restore());
 
   test('that a canary cucumber test is created when the project will be integration tested', async () => {
-    const results = await scaffoldTesting({tests: {integration: true}});
+    mkdir.default.withArgs(`${projectRoot}/test/integration/features`).resolves(pathToCreatedDirectory);
+
+    const results = await scaffoldTesting({projectRoot, tests: {integration: true}});
 
     assert.deepEqual(results.devDependencies, ['http-status-codes']);
+    assert.calledWith(
+      promises.copyFile,
+      resolve(__dirname, '..', 'templates', 'canary.feature'),
+      `${pathToCreatedDirectory}/canary.feature`
+    );
+    assert.calledWith(
+      promises.copyFile,
+      resolve(__dirname, '..', 'templates', 'server-steps.js'),
+      `${pathToCreatedDirectory}/step_definitions/server-steps.js`
+    );
   });
 
   test('that no canary test is created when the project will not be integration tested', async () => {
     const results = await scaffoldTesting({tests: {integration: false}});
 
     assert.deepEqual(results, {devDependencies: []});
+    assert.notCalled(mkdir.default);
+    assert.notCalled(promises.copyFile);
   });
 });
--- src/testing.js
+++ src/testing.js
@@ -1,4 +1,23 @@
-export default async function ({tests}) {
+import {resolve} from 'path';
+import {promises} from 'fs';
+import mkdir from '../thirdparty-wrappers/make-dir';
+
+export default async function ({projectRoot, tests}) {
+  if (tests.integration) {
+    const featuresDirectory = await mkdir(`${projectRoot}/test/integration/features`);
+
+    await Promise.all([
+      promises.copyFile(
+        resolve(__dirname, '..', 'templates', 'canary.feature'),
+        `${featuresDirectory}/canary.feature`
+      ),
+      promises.copyFile(
+        resolve(__dirname, '..', 'templates', 'server-steps.js'),
+        `${featuresDirectory}/step_definitions/server-steps.js`
+      )
+    ]);
+  }
+
   return {
     devDependencies: [...tests.integration ? ['http-status-codes'] : []]
   };
